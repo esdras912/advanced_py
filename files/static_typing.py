a39583e2c99432b00993f4d5dec931d507219d19
esdras912 <martinezesdrascr@gmail.com>
2021-10-08 12:37:08 -0500

    1st file

---
+++ files/static_typing.py
@@ -0,0 +1,20 @@
+from typing import Dict, List, Tuple
+
+num : int = 5
+
+lista : List[int] = [45,45]
+
+dictt : Dict[str,float] = {
+
+    'amogus' : 34.3,
+    'xd' : 45.2
+    
+}
+
+list_dict : List[Dict[str,int]] = [
+{'ID' : 345253}, {'ID' : 342352}
+]
+
+
+tupla : Tuple[int,str,float,bool] = (3,'3',3.0,True)
+print(tupla)
