69ef98c6851bd3303156aa36ad6cea3fe9e6dbed
esdras912 <martinezesdrascr@gmail.com>
2021-10-12 12:22:59 -0500

    1st version

--- files/challege.py
+++ files/challege.py
@@ -1,3 +1,4 @@
+from http.client import RESET_CONTENT
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,27 +11,36 @@ def graph_this(func):
     def wrapper(*args,**kwargs):
         plot_info = func(*args, **kwargs)
         
-        plot_type = plot_info[0] 
-        x_values = plot_info[1]
-        y_values = plot_info[2]
+        plot_type = kwargs['type'] 
         title = kwargs['title']
 
 
+        x_values = plot_info[0]
+        y_values = plot_info[1]
+        
 
+        PLOT_LINE_CODE = f'plt.{plot_type}(x_values, y_values)'
+
+        exec(PLOT_LINE_CODE)
+        plt.title(title)
+
+        plt.savefig(f"{title}.png")
 
 
     return wrapper
 
 
 
-
-def iris_values():
+@graph_this
+def iris_values(*args,**kwargs):
     df = sns.load_dataset('iris')
+
+    df_to_return = pd.DataFrame([df['sepal_length'] , df['petal_length']])
     return [list(df['sepal_length']), list(df['petal_length'])]
 
-graph_this()
 
 
+iris_values(type = 'scatter', title = 'Iris_Data_Set')
 
 
 
